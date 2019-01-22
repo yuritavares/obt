a916eb84dfa620b1020c5dc80728313c69e610b6
Yuri Tavares <y-tavares@hotmail.com>
2019-01-22 10:47:12 -0200

    controllers generated and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token', to: 'user_token#create'
+      resources :follows, only: %i[create destroy]
+      resources :likes, only: %i[create destroy]
+      resources :tweets, only: %i[index show create update destroy]
+      resources :users, only: %i[show create update destroy] do
+        member do
+          get 'following'
+          get 'followers'
+        end
+        get 'current', on: :collection
+      end
     end
   end
 end
