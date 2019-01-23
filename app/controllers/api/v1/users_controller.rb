0671405aa4e526a6fde847052ea866dfc59424e2
Yuri Tavares <y-tavares@hotmail.com>
2019-01-22 23:40:21 -0200

    generated serializers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  
   def create
   end
 
---
+++ app/serializers/api/v1/tweet_serializer.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class TweetSerializer < ActiveModel::Serializer # >
+      attributes :id, :body, :tweet_original_id, :retweets_count, :likes_count, :liked
+      belongs_to :tweet_original
+      belongs_to :user
+
+      def tweet_original
+        options = { each_serializer: Api::V1::TweetSerializer }
+        ActiveModelSerializers::SerializableResource.new(object.tweet_original, options) if object.tweet_original
+      end
+
+      def user
+        options = { each_serializer: Api::V1::UserSerializer }
+        ActiveModelSerializers::SerializableResource.new(object.user, options)
+      end
+
+      def retweets_count
+        object.retweets.count
+      end
+
+      def likes_count
+        object.votes_for.size
+      end
+
+      def liked
+        object.liked_by current_user
+      end
+    end
+  end
+end
