14c34cc71dc274a0be5fdd43037c0cbf3a4c2ac8
Yuri Tavares <y-tavares@hotmail.com>
2019-02-04 21:06:50 -0200

    Rescues activeRecord to json

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,14 @@
-module Api
-  module V1
-    class ApiController < ApplicationController
-      include Knock::Authenticable
-      include CanCan::ControllerAdditions
-    end
-  end
+include Knock::Authenticable
+include CanCan::ControllerAdditions
+
+rescue_from ActiveRecord::RecordNotFound do |msg|
+  render(json: { message: msg }, status: :not_found)
+end
+
+rescue_from ActionController::ParameterMissing do |exception|
+  render(json: { message: exception.param }, status: :bad_request)
+end
+
+rescue_from CanCan::AccessDenied do |msg|
+  render(json: { message: msg }, status: :forbidden)
 end
